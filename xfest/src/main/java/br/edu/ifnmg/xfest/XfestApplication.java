72491ec381292731307045c9a5ed3ef959c1c8cc
WillRenan <willdesign.will@gmail.com>
2023-01-26 20:19:06 -0300

    Ja foi

---
+++ xfest/src/main/java/br/edu/ifnmg/xfest/XfestApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifnmg.xfest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XfestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XfestApplication.class, args);
+	}
+
+}
