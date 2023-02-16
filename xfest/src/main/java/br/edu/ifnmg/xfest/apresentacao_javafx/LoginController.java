d70b54129eb6f0fe867d36bbb6a4dd1603f4ee0b
maxwillias <willias_junior@hotmail.com>
2023-02-16 16:57:00 -0300

    adicionado as imagens e icones as telas

--- xfest/src/main/java/br/edu/ifnmg/xfest/apresentacao_javafx/LoginController.java
+++ xfest/src/main/java/br/edu/ifnmg/xfest/apresentacao_javafx/LoginController.java
@@ -1,8 +1,24 @@
 package br.edu.ifnmg.xfest.apresentacao_javafx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Hyperlink;
 import net.rgielen.fxweaver.core.FxmlView;
 
 @FxmlView("TELA_LOGIN.fxml")
 public class LoginController {
+
+    @FXML
+    private Button btnLoginEntrar;
+
+    @FXML
+    private TextField inpLoginEmail;
+
+    @FXML
+    private TextField inpLoginSenha;
+
+    @FXML
+    private Hyperlink lblCriarConta;
     
 }
