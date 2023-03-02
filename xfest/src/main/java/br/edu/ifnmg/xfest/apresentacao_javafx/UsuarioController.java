9b84b4c5b470a49dd844a56c2d346e0e403bd553
maxwillias <willias_junior@hotmail.com>
2023-03-02 10:30:47 -0300

    metodo sairConta() da tela de usuario, já volta pra tela de login

--- xfest/src/main/java/br/edu/ifnmg/xfest/apresentacao_javafx/UsuarioController.java
+++ xfest/src/main/java/br/edu/ifnmg/xfest/apresentacao_javafx/UsuarioController.java
@@ -4,6 +4,7 @@ import net.rgielen.fxweaver.core.FxmlView;
 
 import org.springframework.stereotype.Service;
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -11,11 +12,16 @@ import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
 @Service
 @FxmlView("TELA_USUARIO.fxml")
-public class UsuarioController {
+public class UsuarioController extends Controller{
+
+    @FXML
+    private BorderPane viewUsuario;
+
     @FXML
     private Label NomeUsuario;
 
@@ -43,5 +49,9 @@ public class UsuarioController {
     @FXML
     private Tab tabServico;
 
+    @FXML
+    public void sairConta(Event event) {
+        carregarScene(viewUsuario, LoginController.class);
+    }
     
 }
