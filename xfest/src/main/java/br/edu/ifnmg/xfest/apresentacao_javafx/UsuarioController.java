fd66b8f1720435f8ba35ddafe00b1f3bbc679fef
WillRenan <willdesign.will@gmail.com>
2023-04-08 02:10:18 -0300

    Interface | Ajustes tela de usuario

--- xfest/src/main/java/br/edu/ifnmg/xfest/apresentacao_javafx/UsuarioController.java
+++ xfest/src/main/java/br/edu/ifnmg/xfest/apresentacao_javafx/UsuarioController.java
@@ -2,10 +2,13 @@ package br.edu.ifnmg.xfest.apresentacao_javafx;
 
 import net.rgielen.fxweaver.core.FxmlView;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Service;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
@@ -20,13 +23,19 @@ import javafx.scene.layout.VBox;
 public class UsuarioController extends Controller{
 
     @FXML
+    private Button btnVoltar;
+
+    @FXML
     private BorderPane viewUsuario;
 
     @FXML
     private Label NomeUsuario;
 
     @FXML
-    private VBox Teste;
+    private VBox vbServicoUsuario;
+
+    @FXML
+    private VBox vbOrdemServicoUsuario;
 
     @FXML
     private Button btnSair;
@@ -49,9 +58,48 @@ public class UsuarioController extends Controller{
     @FXML
     private Tab tabServico;
 
+    
+
+    @FXML
+    void voltarFeedPrincipal(Event event) {
+        carregarScene(viewUsuario, FeedPrincipalController.class);
+    }
+
     @FXML
     public void sairConta(Event event) {
         carregarScene(viewUsuario, LoginController.class);
     }
+
+    @FXML
+    @Override
+    public void initialize() {
+        for (int i = 0; i < 10; i++) {
+            FXMLLoader loader = new FXMLLoader();
+            FXMLLoader loaderOS =  new FXMLLoader();
+
+            loader.setLocation(getClass().getResource("/br/edu/ifnmg/xfest/apresentacao_javafx/CARD_SERVICOS.fxml"));
+            loaderOS.setLocation(getClass().getResource("/br/edu/ifnmg/xfest/apresentacao_javafx/CARD_ORDEM_SERVICOS.fxml"));
+
+
+            VBox vBox;
+            VBox vBoxOS;
+
+            try {
+                vBox = loader.load();
+                vBoxOS = loaderOS.load();
+
+                vbServicoUsuario.getChildren().add(vBox);
+                vbOrdemServicoUsuario.getChildren().add(vBoxOS);
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }   
+        }
+        super.initialize();
+    }
+
+
+
     
 }
